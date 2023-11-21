4104823521ca93f94b46270bcde0adb9304873fa
PILAR <PILAR@DESKTOP-ISL2DJ6>
2023-11-21 12:43:27 +0100

    commin pregunta 1 de práctica

---
+++ examenuf2178/examenuf2178.c
@@ -0,0 +1,8 @@
+/*
+ * examenuf2178.c
+ *
+ *  Created on: 21 nov 2023
+ *      Author: PILAR
+ */
+
+
---
+++ examenuf2178/examenuf2178.h
@@ -0,0 +1,13 @@
+/*
+ * examenuf2178.h
+ *
+ *  Created on: 21 nov 2023
+ *      Author: PILAR
+ */
+
+#ifndef EXAMENUF2178_H_
+#define EXAMENUF2178_H_
+
+
+
+#endif /* EXAMENUF2178_H_ */
